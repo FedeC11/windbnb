08e3c7c52519b3952b8bf505136b783853872cdb
Fede Cuautle <fede.cuautle@gmail.com>
2023-04-13 19:43:56 -0600

    se agrego info del dom y las cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+import data from "./data.js";
+import dom from "./dom.js";
 
+const tarjetas = dom.$('#Tarjetas');
+console.log(tarjetas)
+const datos = await data.getData();
+
+datos.forEach(element => {
+    const card = dom.newCard(element);
+    console.log(card);
+    tarjetas.appendChild(card)
+})
--- js/dom.js
+++ js/dom.js
@@ -2,3 +2,26 @@
 Aqui van todas las funciones o variables relacionadas 
 con la manipulación del DOM en la aplicación
 */
+const $ =selector => document.querySelector(selector); 
+
+const newCard =(obj) => {
+    const div =document.createElement('div');
+    div.className='card-img';
+    div.innerHTML=`
+    <div class="card col ">
+  <img src="${obj.photo}" class="card-img-top" alt="...">
+  <div class="card-body">
+            <span>SUPERHOST</span>
+            <span>${obj.type}</span>
+            <span> ${obj.beds}beds</span>
+            <span>${obj.rating}</span>
+            <h5 class="card-title">${obj.title}</h5>
+          </div>
+    </div>`
+    return div;
+}
+
+export default{
+    newCard,
+    $
+}
